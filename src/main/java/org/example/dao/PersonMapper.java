e37080a528842d6523613d25b56bdd5f94512551
TemirlanKN <temirlan9908@gmail.com>
2021-09-20 17:03:54 +0600

    Push to git created project

---
+++ src/main/java/org/example/dao/PersonMapper.java
@@ -0,0 +1,23 @@
+package org.example.dao;
+
+import org.example.model.Person;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.swing.tree.TreePath;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonMapper implements RowMapper<Person> {
+
+    @Override
+    public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+        Person person = new Person();
+
+        person.setId(resultSet.getInt("id"));
+        person.setName(resultSet.getString("name"));
+        person.setEmail(resultSet.getString("email"));
+        person.setAge(resultSet.getInt("age"));
+
+        return person;
+    }
+}
